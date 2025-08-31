129996bfafea3fed572d34787e799958937b01d9
Donghoon Shin <fkausvery@gmail.com>
2025-08-31 10:37:22 +0900

    hitbox

--- telechips_game_project/shots.c
+++ telechips_game_project/shots.c
@@ -95,9 +95,7 @@ bool shots_add(bool player, bool straight, int x, int y, DIRECTION dir, int powe
             // 플레이어 방향 저장
             switch (dir)
             {
-            //case DIR_UP:    shots[i].dir = SHOT_UP; break;
             case DIR_DOWN:  shots[i].dir = SHOT_DOWN; break;
-            //case DIR_LEFT:  shots[i].dir = SHOT_LEFT; break;
             case DIR_RIGHT: shots[i].dir = SHOT_RIGHT; break;
             }
         }
@@ -135,27 +133,8 @@ bool shots_add(bool player, bool straight, int x, int y, DIRECTION dir, int powe
                     shots[i].dy = 0;
                     shots[i].dir = (SHOT_DIR)SHOT_RIGHT;
                     break;
-                    /*
-                case DIR_UP:
-                    shots[i].dx = 0;
-                    shots[i].dy = -spd;
-                    shots[i].dir = (SHOT_DIR)SHOT_UP;
-                    break;
-                case DIR_DOWN:
-                    shots[i].dx = 0;
-                    shots[i].dy = spd;
-                    shots[i].dir = (SHOT_DIR)SHOT_DOWN;
-                    break;
-                    */
                 }
             }
-            // 랜덤 이동
-            /*else
-            {
-                shots[i].dx = between(-2, 2);
-                shots[i].dy = between(-2, 2);
-            }*/
-
             // 속도가 없으면 바로 종료
             if (!shots[i].dx && !shots[i].dy)
                 return true;
@@ -177,15 +156,9 @@ void shots_update() {
         if (!shots[i].used) continue;
 
         // 플레이어 총알 처리
-        if (shots[i].player) {
-            // 방향에 따라 이동
-            switch (shots[i].dir) {
-            //case SHOT_UP:    shots[i].y -= 5; break;
-            //case SHOT_DOWN:  shots[i].y += 5; break;
-            case SHOT_LEFT:  shots[i].x -= 5; break;
-            case SHOT_RIGHT: shots[i].x += 5; break;
-            }
-
+        if (shots[i].player) {            
+            if (shots[i].dir == SHOT_LEFT)  shots[i].x -= 5;
+            else                            shots[i].x += 5;
             // 화면 밖이면 제거
             if (shots[i].x < -PLAYER_SHOT_WIDTH || shots[i].x > BUFFER_W ||
                 shots[i].y < -PLAYER_SHOT_HEIGHT || shots[i].y > BUFFER_H) {
@@ -196,27 +169,15 @@ void shots_update() {
         else
         {
             shots[i].x += shots[i].dx;
-            shots[i].y += shots[i].dy;
-
-            /*if (shots[i].dir == SHOT_UP) {
-                if (shots[i].y < 0 || shots[i].y <= 110) {
-                    shots[i].used = false;
-                    continue;
-                }
-            }*/
-
-            //else {
-                if ((shots[i].x < -ENEMY_SHOT_WIDTH)
-                    || (shots[i].x > BUFFER_W)
-                    || (shots[i].y < -ENEMY_SHOT_HEIGHT)
-                    || (shots[i].y > BUFFER_H)
-                    ) {
-                    shots[i].used = false;
-                    continue;
-                }
-            //}
+            
+            
+            if ((shots[i].x < -ENEMY_SHOT_WIDTH)
+                || (shots[i].x > BUFFER_W)       
+            ) {
+                shots[i].used = false;
+                continue;
+            }
         }
-
         shots[i].frame++;
     }
 }
@@ -280,14 +241,14 @@ int shots_collide(bool is_player, int x, int y, int w, int h) {
                 sh = ENEMY_SHOT_HEIGHT;
             }
         }
-
-        int hitbox_h = sh / 1.5;                         
+        int hitbox_x = shots[i].x;
+        int hitbox_h = sh * 0.8f;                         
         int hitbox_y = shots[i].y + sh - hitbox_h; 
 
         // 충돌 체크
         if (collide(x, y, x + w, y + h,
-            shots[i].x, hitbox_y,
-            shots[i].x + sw, hitbox_y + hitbox_h))
+            hitbox_x, hitbox_y,
+            hitbox_x + sw, hitbox_y + hitbox_h))
         {
             int damage = shots[i].power;
             shots[i].used = false;
@@ -306,130 +267,92 @@ void shots_draw() {
 
         // 플레이어 총알
         if (shots[i].player) {
-            float t = (float)(shots[i].y - 110) / (PLAYER_MAX_Y - 110);
-            if (t < 0) t = 0;
-            if (t > 1) t = 1;
-            float scale = DEPTH_MIN_SCALE + t * (DEPTH_MAX_SCALE - DEPTH_MIN_SCALE);
-            
-            ALLEGRO_BITMAP* bmp = NULL;
-            JOB_TYPE job = player.job;  // 현재 플레이어 직업
-            if (shots[i].attack_type == ATTACK_NORMAL) 
-            {
-                bmp = sprites.player_shot[job][0]; // 직업별 일반공격
-            }
-            else if (shots[i].attack_type == ATTACK_SKILL_1)
-            {
-                bmp = sprites.player_shot[job][1]; // 직업별 스킬1
-            }
+            ALLEGRO_BITMAP* bmp = (shots[i].attack_type == ATTACK_NORMAL)
+                ? sprites.player_shot[player.job][0]
+                : sprites.player_shot[player.job][1];
 
-            float scale_x = scale;
-            float scale_y = scale;
-            float angle = 0.0f;
+            int w = al_get_bitmap_width(bmp);
+            int h = al_get_bitmap_height(bmp);
 
-            //히트박스 그리기용
-            if (bmp) {
-                float t = (float)(shots[i].y - 110) / (PLAYER_MAX_Y - 110);
-                if (t < 0) t = 0; if (t > 1) t = 1;
-                float scale = DEPTH_MIN_SCALE + t * (DEPTH_MAX_SCALE - DEPTH_MIN_SCALE);
+            float scale = (float)(DEPTH_MIN_SCALE + ((shots[i].y - 110) / (PLAYER_MAX_Y - 110.0f)) *
+                (DEPTH_MAX_SCALE - DEPTH_MIN_SCALE));
 
-                sw = (int)(al_get_bitmap_width(bmp) * scale);
-                sh = (int)(al_get_bitmap_height(bmp) * scale);
+            float final_w = w * scale;
+            float final_h = h * scale;
+
+            if (shots[i].dir == SHOT_LEFT) {
+                al_draw_scaled_bitmap(
+                    bmp,
+                    0, 0, w, h,
+                    shots[i].x + final_w, shots[i].y,
+                    -final_w, final_h,
+                    0
+                );
             }
             else {
-                sw = PLAYER_SHOT_WIDTH;
-                sh = PLAYER_SHOT_HEIGHT;
+                al_draw_scaled_bitmap(
+                    bmp,
+                    0, 0, w, h,
+                    shots[i].x, shots[i].y,
+                    final_w, final_h,
+                    0
+                );
             }
-            //여기까지
-            switch (shots[i].dir)
-            {
-            //case DIR_UP:    angle = 0.0f;               break;
-            //case DIR_DOWN:  angle = ALLEGRO_PI;         break;
-            case DIR_LEFT:  scale_x = -scale; angle = 0.0f;    break;
-            //case DIR_RIGHT: angle = ALLEGRO_PI * 0.5f;  break;
-            }
-
-            float cx = PLAYER_SHOT_WIDTH / 2.0f; //al_get_bitmap_width(bmp) / 2.0f;
-            float cy = PLAYER_SHOT_HEIGHT / 2.0f; //al_get_bitmap_height(bmp) / 2.0f;
-
-            al_draw_scaled_rotated_bitmap(
-                bmp,
-                cx, cy, // 회전 기준 (중심)
-                shots[i].x + cx,
-                shots[i].y + cy,
-                scale_x, scale_y,
-                angle,
-                0
+            int sw = (int)final_w;
+            int sh = (int)final_h;
+            int hitbox_x = shots[i].x;
+            int hitbox_h = sh * 0.8f;
+            int hitbox_y = shots[i].y + sh - hitbox_h;
+            al_draw_rectangle(
+                hitbox_x, hitbox_y,
+                hitbox_x + sw, hitbox_y + hitbox_h,
+                al_map_rgb(255, 0, 0), 1
             );
         }
-        // 적 총알
         else
-        {
-            ALLEGRO_BITMAP* bmp = sprites.enemy_shot;
+        {            
+            ALLEGRO_BITMAP* bmp = (shots[i].power >= 10)
+                ? sprites.enemy_shot[1]
+                : sprites.enemy_shot[0];
 
-            // 적 타입마다 총알 구분
-            if (shots[i].power >= 10) {
-                bmp = sprites.enemy_shot[1]; // 보스 총알
+            int w = al_get_bitmap_width(bmp);
+            int h = al_get_bitmap_height(bmp);
+
+            float scale = (float)(DEPTH_MIN_SCALE + ((shots[i].y - 110) / (PLAYER_MAX_Y - 110.0f)) *
+                (DEPTH_MAX_SCALE - DEPTH_MIN_SCALE));
+
+            float final_w = w * scale;
+            float final_h = h * scale;
+
+            if (shots[i].dir == SHOT_LEFT) {
+                al_draw_scaled_bitmap(
+                    bmp,
+                    0, 0, w, h,
+                    shots[i].x + final_w, shots[i].y,
+                    -final_w, final_h,
+                    0
+                );
             }
             else {
-                bmp = sprites.enemy_shot[0]; // 일반 몹 총알
+                al_draw_scaled_bitmap(
+                    bmp,
+                    0, 0, w, h,
+                    shots[i].x, shots[i].y,
+                    final_w, final_h,
+                    0
+                );
             }
-            //히트박스 그리기용
-
-            if (bmp) {
-                float t = (float)(shots[i].y - 110) / (PLAYER_MAX_Y - 110);
-                if (t < 0) t = 0; if (t > 1) t = 1;
-                float scale = DEPTH_MIN_SCALE + t * (DEPTH_MAX_SCALE - DEPTH_MIN_SCALE);
-
-                sw = (int)(al_get_bitmap_width(bmp) * scale);
-                sh = (int)(al_get_bitmap_height(bmp) * scale);
-            }
-            else {
-                sw = ENEMY_SHOT_WIDTH;
-                sh = ENEMY_SHOT_HEIGHT;
-            }
-            // 깜빡임용 틴트
-            //ALLEGRO_COLOR tint = frame_display
-                //? al_map_rgb_f(1, 1, 1)
-                //: al_map_rgb_f(0.5f, 0.5f, 0.5f);
-
-            // 회전 각도
-            float angle = 0.0f;
-            switch (shots[i].dir) {
-            //case DIR_UP:    angle = 0.0f;               break;
-            //case DIR_DOWN:  angle = ALLEGRO_PI;        break;
-            //case DIR_LEFT:  angle = -ALLEGRO_PI * 0.5f; break;
-            case DIR_RIGHT: angle = ALLEGRO_PI * 0.5f; break;
-            }
-
-            // 깊이 스케일
-            float t = (float)(shots[i].y - 110) / (PLAYER_MAX_Y - 110);
-            if (t < 0) t = 0; else if (t > 1) t = 1;
-            float scale = DEPTH_MIN_SCALE + t * (DEPTH_MAX_SCALE - DEPTH_MIN_SCALE);
-
-            // 중심 좌표
-            float cx = (float)al_get_bitmap_width(bmp);
-            float cy = al_get_bitmap_height(bmp) / 2.0f;
-
-            // 틴트 + 스케일 + 회전 그리기
-            al_draw_tinted_scaled_rotated_bitmap(
-                bmp,
-                //tint,
-                al_map_rgb_f(1, 1, 1),
-                cx, cy, // 회전 기준(중심)
-                shots[i].x, //+ cx, // 화면 X
-                shots[i].y, //+ cy, // 화면 Y
-                scale, scale, // 스케일
-                angle, // 회전
-                0 // 플래그
+            int sw = (int)final_w;
+            int sh = (int)final_h;
+            int hitbox_x = shots[i].x;
+            int hitbox_h = sh *0.8f;
+            int hitbox_y = shots[i].y + sh - hitbox_h;
+            al_draw_rectangle(
+                hitbox_x, hitbox_y,
+                hitbox_x + sw, hitbox_y + hitbox_h,
+                al_map_rgb(255, 0, 0), 1
             );
 
         }
-        int hitbox_h = sh / 1.5;
-        int hitbox_y = shots[i].y + sh - hitbox_h;
-        al_draw_rectangle(
-            shots[i].x, hitbox_y,
-            shots[i].x + sw, hitbox_y + hitbox_h,
-            al_map_rgb(255, 0, 0), 1
-        );
     }
 }
