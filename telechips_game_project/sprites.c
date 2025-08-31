e6f49f4882753406ebac434192753f5aa9b7acda
Donghoon Shin <fkausvery@gmail.com>
2025-08-31 14:32:20 +0900

    boss

--- telechips_game_project/sprites.c
+++ telechips_game_project/sprites.c
@@ -21,23 +21,28 @@ void sprites_init()
     must_init(sprites._sheet, "spritesheet");
 
     sprites.player1 = al_load_bitmap("player.png");
-    sprites.player1_attack = al_load_bitmap("player_attack.png");
+    sprites.player1_attack1 = al_load_bitmap("player_attack1.png");
+    sprites.player1_attack2 = al_load_bitmap("player_attack2.png");
+
     sprites.player2 = al_load_bitmap("player2.png");
-    sprites.player2_attack = al_load_bitmap("player2_attack.png");
+    sprites.player2_attack1 = al_load_bitmap("player2_attack1.png");
+    sprites.player2_attack2 = al_load_bitmap("player2_attack2.png");
 
     // 나중에 직업별/스킬별 공격 나가는 이미지 다르게 설정해야 함
-    sprites.player_shot[JOB_TYPE_1][0] = al_load_bitmap("shot1.png");
-    sprites.player_shot[JOB_TYPE_1][1] = al_load_bitmap("shot2.png");
+    sprites.player_shot[JOB_TYPE_1][0] = al_load_bitmap("player1_shot1.png");
+    sprites.player_shot[JOB_TYPE_1][1] = al_load_bitmap("player1_shot2.png");
     sprites.player_shot[JOB_TYPE_2][0] = al_load_bitmap("player2_shot1.png");
     sprites.player_shot[JOB_TYPE_2][1] = al_load_bitmap("player2_shot2.png");
 
     sprites.enemy[0] = al_load_bitmap("monster01.png");
     sprites.enemy[1] = al_load_bitmap("monster02.png");
-    sprites.enemy[2] = al_load_bitmap("stage1.png");
-    sprites.enemy[3] = al_load_bitmap("boss01.png"); //보스몹 한마리더 
+    sprites.enemy[2] = al_load_bitmap("boss01.png");
+    sprites.enemy[3] = al_load_bitmap("boss02.png"); //보스몹 한마리더 
+
 
     sprites.enemy_shot[0] = al_load_bitmap("enemy_attack.png"); // 일반 몹
     sprites.enemy_shot[1] = al_load_bitmap("boss_shot1.png"); // 보스 몹
+    sprites.enemy_shot[2] = al_load_bitmap("boss_shot2.png"); // 보스 몹
 
     // 배경 이미지
     subway_background = al_load_bitmap("subway_background.png"); 
@@ -52,10 +57,12 @@ void sprites_init()
 void sprites_deinit()
 {
     al_destroy_bitmap(sprites.player1);
-    al_destroy_bitmap(sprites.player1_attack);
+    al_destroy_bitmap(sprites.player1_attack1);
+    al_destroy_bitmap(sprites.player1_attack2);
 
     al_destroy_bitmap(sprites.player2);
-    al_destroy_bitmap(sprites.player2_attack);
+    al_destroy_bitmap(sprites.player2_attack1);
+    al_destroy_bitmap(sprites.player2_attack2);
 
     al_destroy_bitmap(sprites.player_shot[JOB_TYPE_1][0]);
     al_destroy_bitmap(sprites.player_shot[JOB_TYPE_1][1]);
